0feb0fbb5c962c154ebd4aa8107e29253844d754
yahya-mohamoud <yahyamohamoud625@gmail.com>
2025-04-28 17:43:06 +0300

    modified and finished the static design of the page

--- section-3/chefClaude/src/components/Content.jsx
+++ section-3/chefClaude/src/components/Content.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-
 function Content() {
   return (
-    <article className='main-cont'>
-        <input type="text" name="input" id="input" placeholder='e.g.oregano'/>
-        <button type='submit'>+Add ingredient</button>
-    </article >
+    <main className='main-cont'>
+        <form action="">
+            <input type="text" name="input" id="input" placeholder='e.g.oregano'/>
+            <button type='submit'>Add ingredient</button>
+        </form>
+    </main >
   )
 }
 
