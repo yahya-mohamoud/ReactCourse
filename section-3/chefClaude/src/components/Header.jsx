1e264aa1edf1c702a843be8b222134a1c1ac69c5
yahya-mohamoud <yahyamohamoud625@gmail.com>
2025-04-28 16:58:59 +0300

    created the header component of the project

---
+++ section-3/chefClaude/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <nav>
+            <img src="../src/assets/image.png" alt="chef-logo" />
+            <h1>Chef Claude</h1>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
