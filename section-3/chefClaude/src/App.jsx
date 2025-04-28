c4c7888c7fdff2b3f826de95f7e709ea35c7dab4
yahya-mohamoud <yahyamohamoud625@gmail.com>
2025-04-28 17:15:05 +0300

    created the content component and modified a little

--- section-3/chefClaude/src/App.jsx
+++ section-3/chefClaude/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header'
+import Content from './components/Content'
 
 function App() {
   return (
     <div>
       <Header />
+      <Content />
     </div>
   )
 }
---
+++ section-3/chefClaude/src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Content() {
+  return (
+    <article className='main-cont'>
+        <input type="text" name="input" id="input" placeholder='e.g.oregano'/>
+        <button type='submit'>+Add ingredient</button>
+    </article >
+  )
+}
+
+export default Content
