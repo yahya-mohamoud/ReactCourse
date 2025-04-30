c2db87c45fe0668594b1e77897b4f51a4ff5b7e7
yahya-mohamoud <yahyamohamoud625@gmail.com>
2025-04-30 07:54:12 +0300

    added a little concept in my chefClaude project

--- section-3/chefClaude/src/components/Content.jsx
+++ section-3/chefClaude/src/components/Content.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+
 function Content() {
+  const [ingredients, setIngredients] = useState([
+    'Chicken',
+    'Oregano',
+    "Tomatoes"
+  ])
+
+  const items = ingredients.map((ingredient) => <li key={ingredient}>{ingredient}</li>)
+
+  const handleSubmit = (event) =>{
+      event.preventDefault();
+      console.log("submitted");
+      const formData = new FormData(event.currentTarget)
+      const newIngredient = formData.get('ingredient')
+      setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+      console.log(ingredients);
+      
+  }
   return (
     <main className='main-cont'>
-        <form action="">
-            <input type="text" name="input" id="input" placeholder='e.g.oregano'/>
-            <button type='submit'>Add ingredient</button>
+        <form action="" onSubmit={handleSubmit}>
+            <input type="text" name="ingredient" id="input" placeholder='e.g.oregano'/>
+            <button type='submit' >
+              Add ingredient
+            </button>
         </form>
+        <ul>
+          {items}
+        </ul>
     </main >
   )
 }
