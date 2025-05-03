17a5fc4dfa4ed706cbb524bd88e6f0e97d65bac0
yahya-mohamoud <yahyamohamoud625@gmail.com>
2025-05-03 21:25:34 +0300

    created two components and tried to clean the content component

---
+++ section-3/chefClaude/src/components/IngredientLists.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function IngredientLists({ingredients, handleClick, items}) {
+  return (
+    <section>
+    <h2>Ingredients on hand:</h2>
+  {ingredients.length > 0 &&  
+    <ul className="ingredientList">{items}</ul>}
+     
+   {ingredients.length  == 4 && <div className="get-recipe">
+          <div>
+           <h3>Ready for a recipe</h3>
+           <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button onClick={handleClick}>Get a recipe</button>
+    </div>
+  }
+  </section>
+  )
+}
+
+export default IngredientLists
